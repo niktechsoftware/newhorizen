ae53ab238c28298dcd75cb3aadc7c07c5a58b2d8
Samrat Shukla <atreeshukla139@gmail.com>
2020-01-25 11:09:51 +0530

    change content

--- application/views/welcome_message.php
+++
@@ -1,5 +0,0 @@
-
-<?php $this->load->view("header");?>
-<?php $this->load->view("slider");?>
-<?php $this->load->view("body");?>
-<?php $this->load->view("footer");?>
---
+++ nha/application/views/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+<title>New 	Horizon Acadmy</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link href="<?php echo base_url();?>assets/https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+<link rel="stylesheet" href="<?php echo base_url();?>assets/css/open-iconic-bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo base_url();?>assets/css/animate.css">
+<link rel="stylesheet" href="<?php echo base_url();?>assets/css/owl.carousel.min.css">
+<link rel="stylesheet" href="<?php echo base_url();?>assets/css/owl.theme.default.min.css">
+<link rel="stylesheet" href="<?php echo base_url();?>assets/css/magnific-popup.css">
+<link rel="stylesheet" href="<?php echo base_url();?>assets/css/aos.css">
+<link rel="stylesheet" href="<?php echo base_url();?>assets/css/ionicons.min.css">
+<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap-datepicker.css">
+<link rel="stylesheet" href="<?php echo base_url();?>assets/css/jquery.timepicker.css">
+<link rel="stylesheet" href="<?php echo base_url();?>assets/css/flaticon.css">
+<link rel="stylesheet" href="<?php echo base_url();?>assets/css/icomoon.css">
+<link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+<div class="container">
+<a class="navbar-brand" href="<?php echo base_url();?>assets/index.html"><i class="flaticon-university"></i>NEW HORIZON<br><small>ACADEMY</small></a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+<span class="oi oi-menu"></span> Menu
+</button>
+
+<nav>
+	<div>
+		<div>
+<div class="collapse navbar-collapse" id="ftco-nav">
+<ul class="navbar-nav ml-auto">
+<li class="nav-item active"><a href="<?php echo base_url();?>index.php/welcome" class="nav-link">Home</a></li>
+<li class="nav-item"><a href="<?php echo base_url();?>index.php/welcome/about" class="nav-link">About</a></li>
+<li class="nav-item"><a href="<?php echo base_url();?>assets/course.html" class="nav-link">Courses</a></li>
+<li class="nav-item"><a href="<?php echo base_url();?>assets/teacher.html" class="nav-link">Teacher</a></li>
+<li class="nav-item"><a href="<?php echo base_url();?>assets/blog.html" class="nav-link">Blog</a></li>
+<li class="nav-item"><a href="<?php echo base_url();?>assets/event.html" class="nav-link">Events</a></li>
+<li class="nav-item"><a href="<?php echo base_url();?>assets/contact.html" class="nav-link">Contact</a></li>
+<li class="nav-item cta"><a href="<?php echo base_url();?>assets/contact.html" class="nav-link"><span>Apply Now!</span></a></li>
+</ul>
+</div>
+</div>
+</nav>
+</div>
+</nav>
+</div>
+</nav>
+</body>
+</html>
